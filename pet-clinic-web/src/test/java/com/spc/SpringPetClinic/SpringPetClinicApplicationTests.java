323834f4bbd8e08592cbf5669c1f4e7eb858450b
bradygrapentine <brady.grapentine@gmail.com>
2021-10-15 15:55:28 -0400

    configuring for maven release:perform

--- pet-clinic-web/src/test/java/com/spc/SpringPetClinic/SpringPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/spc/SpringPetClinic/SpringPetClinicApplicationTests.java
@@ -3,7 +3,8 @@ package com.spc.SpringPetClinic;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes={})
+//@SpringBootTest
 class SpringPetClinicApplicationTests {
 
 	@Test
